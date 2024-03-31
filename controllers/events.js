9e68c7fc395b5ecc511e7c04d02dd842e05eaa7a
joemiddie13 <joemiddie13@gmail.com>
2024-03-30 20:27:14 -0700

    added createdAt, footer, and boostrap theme

--- controllers/events.js
+++ controllers/events.js
@@ -25,11 +25,14 @@ module.exports = function (app, models) {
   // SHOW
   app.get('/events/:id', (req, res) => {
     models.Event.findByPk(req.params.id, { include: [{ model: models.Rsvp }] }).then(event => {
+        let createdAt = event.createdAt;
+        createdAt = moment(createdAt).format('MMMM Do YYYY, h:mm:ss a');
+        event.createdAtFormatted = createdAt;
         res.render('events-show', { event: event });
     }).catch((err) => {
         console.log(err.message);
     })
-  });
+});
 
   // EDIT
   app.get('/events/:id/edit', (req, res) => {
