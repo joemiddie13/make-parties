08b6ab5595d1e0b84fdf26b62101a7753b51546b
joemiddie13 <joemiddie13@gmail.com>
2024-03-30 20:15:50 -0700

    Users can delete rsvps

--- app.js
+++ app.js
@@ -10,27 +10,31 @@ const exphbs = require('express-handlebars');
 const Handlebars = require('handlebars')
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 
+// Parse incoming request bodies
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 require('./controllers/events')(app, models);
 require('./controllers/rsvps')(app, models);
 
 // Use "main" as our default layout
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
+
 // Use handlebars to render
 app.set('view engine', 'handlebars');
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 
 // Render the "home" layout for the main page and send the following msg
 // app.get('/', (req, res) => {
-//    res.render('home', { msg: 'Handlebars are Cool!' });
-//  })
+//   res.render('home', { msg: 'Handlebars are Cool!' });
+// })
 
 // OUR MOCK ARRAY OF PROJECTS
 var events = [
-   { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-   { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://images.unsplash.com/photo-1552053831-71594a27632d?q=80&w=1324&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-   { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://images.unsplash.com/photo-1559190394-df5a28aab5c5?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
- ]
+  { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+  { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://images.unsplash.com/photo-1552053831-71594a27632d?q=80&w=1324&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+  { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://images.unsplash.com/photo-1559190394-df5a28aab5c5?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
+]
 
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
--- controllers/rsvps.js
+++ controllers/rsvps.js
@@ -8,13 +8,33 @@ module.exports = (app, models) => {
 
   // CREATE
   app.post('/events/:eventId/rsvps', (req, res) => {
+    if (!req.body || !req.body.name || !req.body.email) {
+      res.status(400).send('Name and email are required');
+      return;
+    }
+
     req.body.EventId = req.params.eventId;
     models.Rsvp.create(req.body).then(rsvp => {
       res.redirect(`/events/${req.params.eventId}`);
     }).catch((err) => {
-        console.log(err)
+      console.log(err);
+      res.status(500).send('Internal Server Error');
     });
   });
 
-  // DESTROY  
-}
+  // DELETE
+  app.delete('/events/:eventId/rsvps/:id', (req, res) => {
+    models.Rsvp.findByPk(req.params.id).then(rsvp => {
+      if (!rsvp) {
+        res.status(404).send('RSVP not found');
+        return;
+      }
+
+      rsvp.destroy();
+      res.redirect(`/events/${req.params.eventId}`);
+    }).catch((err) => {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    });
+  });
+};
