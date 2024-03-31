dff7f069a5f440bde19a0cf9c25d799ec7f08847
joemiddie13 <joemiddie13@gmail.com>
2024-03-31 12:50:41 -0700

    views for logged in users

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 // Initialize express
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const models = require('./db/models');
-const methodOverride = require('method-override')
-const setupAuthRoutes = require('./controllers/auth')
-const cookieParser = require('cookie-parser')
+const methodOverride = require('method-override');
+const setupAuthRoutes = require('./controllers/auth');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 // require handlebars
 const exphbs = require('express-handlebars');
-const Handlebars = require('handlebars')
-const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const Handlebars = require('handlebars');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
 
 // Parse incoming request bodies
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -35,3 +36,41 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('App listening on port 3000!')
 })
+
+app.use(function authenticateToken(req, res, next) {
+  // Gather the jwt access token from the cookie
+  const token = req.cookies.mpJWT;
+
+  if (token) {
+    jwt.verify(token, "AUTH-SECRET", (err, user) => {
+      if (err) {
+        console.log(err);
+        // redirect to login if not logged in and trying to access a protected route
+        res.redirect('/login');
+      }
+      req.user = user;
+      next(); // pass the execution off to whatever request the client intended
+    });
+  } else {
+    next();
+  }
+});
+
+app.use((req, res, next) => {
+  // if a valid JWT token is present
+  if (req.user) {
+    // Look up the user's record
+    models.User.findByPk(req.user.id)
+      .then(currentUser => {
+        // make the user object available in all controllers and templates
+        res.locals.currentUser = currentUser;
+        next();
+      })
+      .catch(err => {
+        console.log(err);
+        next();
+      });
+  } else {
+    next();
+  }
+});
--- controllers/events.js
+++ controllers/events.js
@@ -4,6 +4,7 @@ module.exports = function (app, models) {
   app.get('/', (req, res) => {
     models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
       res.render('events-index', { events: events });
+      console.log(req.user);
     });
   });
 
